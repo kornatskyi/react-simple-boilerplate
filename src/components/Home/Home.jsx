66272f49b5dac9d2efda1893ab5958951e118e70
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-07 17:02:24 -0700

    basic setUp. Scss

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Home.scss"
+
+
+export default function Home() {
+    return (
+    <div className="container">
+            <h1>This is a Home component</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
+import "./styles/style.scss"
+
 import App from "./App.js";
 
 
