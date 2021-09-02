7b111f2c7ee055d3ae72df0d86633752b5401ba0
Bohdan <bohdan.kornatskyi@gmail.com>
2021-09-01 20:50:08 -0700

    init for canvas branch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
 import './index.css';
-import "./styles/style.scss"
-
-import App from "./App.js";
-
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+import "./styles/style.scss";
